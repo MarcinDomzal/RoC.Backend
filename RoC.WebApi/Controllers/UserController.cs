e12a4dda00eecd30291d98a3b8261ec7d82ea22e
BLACKHAWK\MarcinD <marcin.domzal@zefcom.pl>
2025-04-09 23:15:02 +0200

    Kontroler i akcja rejestracji konta

---
+++ RoC.WebApi/Controllers/UserController.cs
@@ -0,0 +1,23 @@
+﻿using MediatR;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using RoC.Application.Logic.User;
+
+namespace RoC.WebApi.Controllers
+{
+    [Route("[controller]/[action]")]
+    [ApiController]
+    public class UserController : BaseController
+    {
+        public UserController(ILogger logger, IMediator mediator) : base(logger, mediator)
+        {
+        }
+
+        [HttpPost]
+        public async Task<ActionResult> CreateUserWithAccount([FromBody] CreateUserWithAccountCommand.Request model)
+        {
+            var createAccountResult = await _mediator.Send(model);
+            return Ok(createAccountResult);
+        }
+    }
+}
