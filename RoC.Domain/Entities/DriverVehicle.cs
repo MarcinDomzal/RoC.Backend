934640ae577636b471aa8345c64e0f336a0b62ce
BLACKHAWK\MarcinD <marcin.domzal@zefcom.pl>
2025-04-14 19:21:01 +0200

    Add DriverVehicle

---
+++ RoC.Domain/Entities/DriverVehicle.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RoC.Domain.Entities
+{
+    public class DriverVehicle
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public DateTime ControlDay { get; set; }
+        public DateTime ExamDay { get; set; }
+        public DateTime PeriodDay { get; set; }
+        public DateTime NextDay { get; set; }
+        public Boolean IsPassed { get; set; }
+        public string Description { get; set; }
+
+    }
+}
